bafc86f29c13aeb5e53a1b57e1ade29597125969
Alex Gavazov <alex@gavazov.net>
2025-10-19 20:18:03 +0000

    Update

--- laravel/resources/views/virtual-project-manager/index.blade.php
+++ laravel/resources/views/virtual-project-manager/index.blade.php
@@ -7,7 +7,52 @@
 
   <h1 class="h3 mb-3">AI Virtual Project Manager</h1>
 
-  <button class="btn btn-primary" id="js-start-btn">START</button>
+  <div class="card">
+    <div class="card-body">
+      <div class="row gy-2">
+        <div class="col-12">
+          <h2 class="h5 pb-2 border-bottom border-dashed">Setup your assistant</h2>
+        </div>
+
+        <div class="col-12 col-md-6 col-xl-3">
+          <label class="app-form-label required" for="f-name">Your name</label>
+          <input class="form-control form-control-lg" id="f-name" type="text" placeholder="Alex Gavazov..." required/>
+        </div>
+
+        <div class="col-12 col-md-6 col-xl-3">
+          <label class="app-form-label" for="f-role">Your role</label>
+          <select class="form-select form-select-lg" id="f-role">
+            <option value="engineer">Engineer</option>
+            <option value="designer" disabled>Designer (coming soon)</option>
+            <option value="qa" disabled>QA (coming soon)</option>
+            <option value="other" disabled>Other (coming soon)</option>
+          </select>
+        </div>
+
+        <div class="col-12 col-md-6 col-xl-3">
+          <label class="app-form-label" for="f-assistantBehaviour">Assistant behaviour</label>
+          <select class="form-select form-select-lg" id="f-assistantBehaviour">
+            <option value="normal">Sam - Normal person</option>
+            <option value="focused">Ray - Straight-to-the-point</option>
+            <option value="crazy">Karen - Crazy nervous woman</option>
+          </select>
+        </div>
+
+        <div class="col-12 col-md-6 col-xl-3">
+          <label class="app-form-label" for="f-assistantBehaviour">Language</label>
+          <select class="form-select form-select-lg" id="f-assistantBehaviour">
+            <option value="english">English</option>
+            <option value="bulgarian">Bulgarian</option>
+            <option value="german">German (not tested)</option>
+          </select>
+        </div>
+
+        <div class="col-12">
+          <button class="btn btn-primary btn-lg mt-3" id="js-start-btn" type="button">Start</button>
+        </div>
+      </div>
+    </div>
+  </div>
 
   <div id="audio-visualizer" style="width: 100%; height: calc(100vh - 500px);"></div>
   <pre id="action-log"></pre>
